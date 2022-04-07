127bd6cc97aea0e78864bf5723ec80014b0f683c
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-04-07 11:07:10 +0200

    refactor:#2 Write fonction in JS vanilla.

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-function createFeed(resultatFetch){ // result de fetch
+//Je déclare dans une variable const l'url de mon API
+const API_URL = "https://api.spaceflightnewsapi.net/v3/blogs";
 
-    for(let i = 0; i < resultatFetch.length; i++){ // Je fais une boucle sur le result de fetch
-        let title = resultatFetch[i].title; // Je déclare une varaible title et je lui mets comme valeur la valeur qui se trouve a params[i] je récupere le title.
-        let image = resultatFetch[i].imageUrl;
-        let publish = resultatFetch[i].publishedAt;
-
-        let newDiv = $("<div>"); // Je déclare une variable newDiv qui via jquery va me créer une nouvelle div
-        /**
-         * Dans chaqu'une des newDiv en html 
-         * mettre un h3 avec la valeur de tilte récupérer au dessus
-         * et ainsi de suite. !!! Il faut utilisé syntaxe de jquery  */        
-        $newDiv.html(`<h3>${title}</h3> 
-        <img src=${image} alt="photo" widht="80">
-        <p>${publish}</p>`);
-        $("main").append($newDiv);
-        // je selectionne la balise main, appele la méthode append() qui ajoute une div a chaque tour de boucle dans main
-    }
-}
-/**
- * Je déclare dans une variable const l'url de mon API
- */
-const API_URL = "https://api.spaceflightnewsapi.net/v3/blogs"; 
-/**
- * Récupérer donnée de la base de  l'API
- */
 fetch(API_URL)
-
-.then(response => response.json())
-.then(result => { 
-    createFeed(result); 
-});
-
-
-
-
-
-
-
-
-
-
-
+    .then(response => response.json())
+    .then(blogs => {
+        console.log(blogs);
+        createFeed(blogs);
+    });
 /**
  * La méthode .then() appel le resultat de la methode fetch le navigateur recoit une réponse on la
  * transforme en response.json pour qu'elle soit lisible.
  */
+
+ function createFeed(resultatFetch) { // result de fetch
+
+    for (let i = 0; i < resultatFetch.length; i++) {
+        let title = resultatFetch[i].title;
+        let summary = resultatFetch[i].summary;
+        let url = resultatFetch[i].url;
+
+        // Create div 
+        let newDiv = document.createElement("div");
+        document.querySelector("main").append(newDiv);
+
+        // Create Title in newDiv
+        let titre = document.createElement("h3");
+        titre.innerHTML= title;
+        newDiv.append(titre);
+ 
+        //Create summary in newDiv
+        let resume = document.createElement("p");
+        resume.innerHTML = summary;
+        newDiv.appendChild(resume)
+
+        //Create url in newDiv
+        let urlLink = document.createElement("p");
+        urlLink.innerHTML = url;
+        newDiv.appendChild(urlLink);
+        
+        
+        
+        //newDiv.innerHTML = ("<p>");
+        //let h = newDiv.createElement("h3");
+        /*let img = newDiv.createElement("").append({ image });
+        let publ = newDiv.createElement("p").append({ publish });*/
+        //document.querySelector("main").append(newDiv);
+        //document.querySelector("main").append(newDiv).appendChild(h, img, publ);
+        // je selectionne la balise main, appele la méthode append() qui ajoute une div a chaque tour de boucle dans main
+    }
+}
