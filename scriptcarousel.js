879f30a6ccb642ca281fa0d3cfe60627d2648ae1
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-04-08 10:39:05 +0200

    feat:#3 Add button on the slider

--- scriptcarousel.js
+++ scriptcarousel.js
@@ -1,5 +1,38 @@
-$(function() {
-    $(function() {
+(function ($) {
+    $(function () {
+        var jcarousel = $('.jcarousel');
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 350) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+        $('.jcarousel').jcarouselAutoscroll({
+            target: '+=1'
+        });
+
+        $('.jcarousel-prev').jcarouselControl({
+            cible: '-=1'
+        });
+    
+        $('.jcarousel-next').jcarouselControl({
+            cible: ''
+        });
+    });
+})(jQuery);
+
+$(function () {
+    $(function () {
         $('.jcarousel').jcarousel({
             // Configuration goes here
         });
@@ -7,9 +40,17 @@ $(function() {
     $('.jcarousel').jcarousel({
         wrap: 'circular'
     });
-     
+
     $('.jcarousel').jcarouselAutoscroll({
         target: '+=1',
         autostart: true,
+    });
+
+    $('.jcarousel-prev').jcarouselControl({
+        cible: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        cible: '+=2'
     });
 });
