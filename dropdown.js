62f57100d4c3a4226e5c07af437428a2b961f462
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-04-11 08:45:37 +0200

    refactor: REfacto

--- dropdown.js
+++ dropdown.js
@@ -1,16 +1,16 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function clickDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+      //const i;
       for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+        const openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
