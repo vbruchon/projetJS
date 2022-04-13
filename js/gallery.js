9845625bde1fd4547831c9d770386930c63c9360
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-04-13 09:58:16 +0200

    feat:#6 Delete an Dom Element, and refresh API pictures but no refresh the page

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,42 @@
-const Url_Api = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?" +
-    "sol=1000&" +
-    "api_key=vT8epvedUmKLuLHpODnwhbukvAbkgyf6zT4E8nHd";
 
-const req = new Request(Url_Api);
+/*------------------------------------Actualisation API via bouton refresh----------------------------*/
+let refresh = document.getElementById("refresh");
 
-fetch(req)
-    .then((response) => response.json())
-    .then((images) => {
-        console.log(images);
-        //console.log(images.photos[2].img_src);
-        createGallery(images);
-    });
+refresh.onclick = function () {
+    connexionAPI();
+}
+/*-------------------------------------------FIN Actualisation API--------------------------------------*/
 
 
+/*----------------------------------------------Connexion_API------------------------------------------*/
+function connexionAPI() {
+    const Url_Api = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?" +
+        "sol=1000&" +
+        "api_key=vT8epvedUmKLuLHpODnwhbukvAbkgyf6zT4E8nHd";
+
+    const req = new Request(Url_Api);
+
+    fetch(req)
+        .then((response) => response.json())
+        .then((images) => {
+            console.log(images);
+            //console.log(images.photos[2].img_src);
+            createGallery(images);
+            //eventListennerToTheClick();
+        });
+}
+/*----------------------------------------FIN_Connexion_API--------------------------------------------*/
+
+
+/*---------------------------------------Création_de_la_Gallery----------------------------------------*/
 function createGallery(images) {
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 3; i++) {
         url_img = images.photos[i].img_src;
 
         // Create section 
         let newSection = document.createElement("section");
-        newSection.className = 'gallery';
+        newSection.className = 'gallery-' + i;
         document.querySelector("main").append(newSection);
 
         // Create div in section
@@ -38,19 +54,25 @@ function createGallery(images) {
         let deleteGalery = document.createElement("button");
         deleteGalery.className = "delete_button";
         deleteGalery.textContent = "Delete";
+        //equivaut à addeventlistenner du bas//
+        deleteGalery.onclick = function () { newSection.remove() }
         divNew.appendChild(deleteGalery);
     }
 }
+/*-----------------------------------FIN_Création_de_la_Gallery----------------------------------------*/
 
-btn_delete = document.getElementsByClassName("delete_button");
-console.log(btn_delete);
 
-for (let i = 0; i < btn_delete.length; i++) {
-    btn_delete[i].addEventListener("click", (event) => {
-        event.preventDefault();
-        console.log(event);
-    })
+/*-------------------------------Event_Listener_For_The_Button_delete----------------------------------
+function eventListennerToTheClick() {
+    btn_delete = document.getElementsByClassName("delete_button");
+    console.log(btn_delete);
 
+    for (let i = 0; i < btn_delete.length; i++) {
+        btn_delete[i].addEventListener("click", (event) => {
+            event.preventDefault();
+            document.getElementById("img").remove();
+            document.getElementsByClassName
+        })
+    }
 }
-
-
+-------------------------------FIN_Event_Listener_For_The_Button_delete------------------------------*/
